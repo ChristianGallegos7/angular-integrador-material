93003f1b54f0f647db11b6bf119dfa3a62b7307b
Pxlarized <chriz@liz.com>
2024-07-26 11:46:25 -0700

    Lazy loading en rutas de login y registro ✔

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './src/components/welcome/welcome.component';
-import { LoginComponent } from './src/components/auth/login/login.component';
-import { RegistroComponent } from './src/components/auth/registro/registro.component';
 
 export const routes: Routes = [
     {
         path: '', component: WelcomeComponent
     },
     {
-        path: 'login', component: LoginComponent
+        path: 'login', loadComponent: () => import('./src/components/auth/login/login.component').then(m => m.LoginComponent)
     },
     {
-        path: 'registro', component: RegistroComponent
+        path: 'registro', loadComponent: () => import('./src/components/auth/registro/registro.component').then(m => m.RegistroComponent)
     }
 ];
