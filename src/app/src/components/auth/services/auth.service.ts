477fe57f794eb19c13a721542d74cb064f92a7a9
Pxlarized <chriz@liz.com>
2024-07-28 08:09:58 -0700

    auth service

---
+++ src/app/src/components/auth/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private user?: User;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  get currentUser(): User | undefined {
+    if (!this.user) return undefined;
+    return structuredClone(this.user);
+  }
+
+  login(email: string, password: string): Observable<User> {
+    this.http
+  }
+
+
+
+
+}
